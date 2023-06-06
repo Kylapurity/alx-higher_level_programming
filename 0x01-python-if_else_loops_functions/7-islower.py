0f79659ad40fa4aacbcf2b48bad5b91baacbeedc
Kylapurity <kihiupurity29@gmail.com>
2023-06-06 15:38:33 -0700

    upper

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-def uppercase(str):
-    new_string = ""
-    for char in str:
-        if (ord(char) in range(97, 123)):
-            new_string += chr(ord(char) - 32)
-        else:
-            new_string += char
-    print("{new_string}".format(new_string=new_string))
+def islower(c):
+    x = ord(c)
+    if x in range(97, 123):
+        return(True)
+    else:
+        return(False)
