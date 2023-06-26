bde5266184328c7d48aaff992539a090a7320632
Kylapurity <kihiupurity29@gmail.com>
2023-06-26 15:47:42 -0700

    selvat

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    while (i < x):
+    total = 0
+    for i in range(x):
         try:
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
+            total += 1
         except IndexError:
             break
-        i += 1
     print("")
-    return(i)
+    return (total)
