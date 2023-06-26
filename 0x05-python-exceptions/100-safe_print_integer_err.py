5ec5683c9bc48b44a943162361830ba128f00d59
Kylapurity <kihiupurity29@gmail.com>
2023-06-26 15:40:23 -0700

    seven

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 import sys
 
-def safe_function(fct, *args):
+
+def safe_print_integer_err(value):
     try:
-        result = fct(*args)
-        return (result)
-    except:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return (False)
