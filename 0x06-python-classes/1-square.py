9b9809aaa7be1e7ed7eafb75e2bfbd40a9422e23
Kylapurity <kihiupurity29@gmail.com>
2023-06-27 16:04:06 -0700

    one

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Make an empty a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    
+    def __int_(self,size):
+      """Intialize a new square.
+       Args:
+       size (int): The size of a new square.
+       """
+       self.__size =size
