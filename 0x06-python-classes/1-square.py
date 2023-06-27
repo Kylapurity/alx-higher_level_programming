f8f4fe18486440f0faa8381e8a5cc3cb87353422
Kylapurity <kihiupurity29@gmail.com>
2023-06-27 16:12:59 -0700

    one

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Make an empty a class Square."""
+'''Defined a class called Square which represents a aquare'''
 
 
 class Square:
-    """Represents new sqaure."""
-     def __init__(self, size):
-        """Initializes instances of Square.
+    '''Square class with private size attr'''
+
+    def __init__(self, size):
+        '''Initializes instances of Square.
 
            Args:
                 size (int): Represents the size of one side of the square.
-        """
+        '''
         self.__size = size
