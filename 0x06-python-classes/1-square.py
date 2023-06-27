3ddc8c7fcda01ee9bfa77a852ca0de74144f2b5f
Kylapurity <kihiupurity29@gmail.com>
2023-06-27 16:11:52 -0700

    one

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,11 @@
 
 
 class Square:
-    """Represent a square."""
-    
-def __init__(self, size):
-        '''Initializes instances of Square.
+    """Represents new sqaure."""
+     def __init__(self, size):
+        """Initializes instances of Square.
 
            Args:
                 size (int): Represents the size of one side of the square.
-        '''
+        """
         self.__size = size
